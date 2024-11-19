0d987559f4b071fdb4ef3c285f6c81dbaf25c87a
DragonStrike0 <nael.dowlut@efrei.net>
2024-11-19 01:51:30 +0100

    Finalising the structures and basic functions to create nodes and tree.

--- tree.c
+++ tree.c
@@ -1,14 +1,25 @@
 #include "tree.h"
 
 
-r_node starting(Position start, int initial_cost){
-    r_node *root = malloc(sizeof(r_node));
-    root->pos = start;
-    root->cost = initial_cost;
-    root->children = malloc(sizeof(r_node)*9);
-    return *root;
+
+t_tree* create_tree(t_localisation start_loc, int start_cost, int max_depth) {
+    t_tree* tree = malloc(sizeof(t_tree));
+    tree->max_depth = max_depth;
+    tree->total_nodes = 0;
+    tree->root = create_node(start_loc, start_cost, NO_MOVE, NULL, 0);
+    return tree;
 }
 
-
+t_node* create_node(t_localisation loc, int cost, t_move move, t_node* parent, int depth) {
+    t_node* node = malloc(sizeof(t_node));
+    node->loc = loc;
+    node->cost = cost;
+    node->last_move = move;
+    node->parent = parent;
+    node->children = NULL;
+    node->child_count = 0;
+    node->depth = depth;
+    return node;
+}
 
 
--- tree.h
+++ tree.h
@@ -1,24 +1,47 @@
 #include "stack.h"
 #include "moves.h"
-#include "stack.h"
 #include "loc.h"
 #include "stdlib.h"
-
 #ifndef UNTITLED1_TREE_H
 #define UNTITLED1_TREE_H
 
 typedef struct {
-    int x,y;
-    char orientation;
-} Position;
+    t_move moves[9];
+    int count_moves;
+}t_avlbl_moves;
 
-typedef struct r_node {
-    Position pos;
+typedef struct node{
+    t_localisation loc;
     int cost;
-    int move;
+    t_move last_move;
+    struct node* parent;
+    struct node** children;
+    int child_count;
     int depth;
-    struct r_node** children;
-} r_node;
+}t_node;
+
+typedef struct {
+    t_node *root;
+    int max_depth;
+    int total_nodes;
+}t_tree;
+
+
+
+t_tree* create_tree(t_localisation start_loc, int start_cost, int max_depth);
+
+t_node* create_node(t_localisation loc, int cost, t_move move, t_node* parent, int depth);
+
+void add_child(t_node* parent, t_node* child);
+
+void build_tree_level(t_node* node, t_avlbl_moves* available_moves, int terrain_costs[MAX_Y][MAX_X]);
+
+t_node* find_min_cost_leaf(t_tree* tree);
+
+void get_path_to_node(t_node* node, t_move* moves, int* move_count);
+
+void free_tree(t_tree* tree);
+
 
 
 
